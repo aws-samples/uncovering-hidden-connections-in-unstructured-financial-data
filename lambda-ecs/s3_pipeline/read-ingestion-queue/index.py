5235ccb8a7c7e172a756eeadede961b8035f2c0a
Xan Huang <xan.huang@gmail.com>
2024-07-08 17:58:32 +0800

    added filename to step function execution name for easy identification

--- lambda-ecs/s3_pipeline/read-ingestion-queue/index.py
+++ lambda-ecs/s3_pipeline/read-ingestion-queue/index.py
@@ -1,6 +1,7 @@
 import json
 import boto3
 import os
+import uuid
 
 sqs = boto3.client('sqs', region_name=os.environ["AWS_REGION"])
 queue_url = sqs.get_queue_url(QueueName=os.environ["QUEUE_NAME"])['QueueUrl']
@@ -14,9 +15,11 @@ def lambda_handler(event, context):
             MaxNumberOfMessages=1,
             WaitTimeSeconds=1
         )
+        s3_key = json.loads(response["Messages"][0]["Body"])["S3_KEY"]
         execution_input = {"Messages": response["Messages"]}
         response = step_functions.start_execution(
             stateMachineArn=state_machine_arn,
+            name=s3_key+"_"+str(uuid.uuid4()),
             input=json.dumps(execution_input)
         )
     except Exception as e:
