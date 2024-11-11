8a34cf2def00bbf1966550102f1bf99fa68dfd32
Xan Huang <xan.huang@gmail.com>
2024-11-11 22:39:41 +0800

    update command from cdk to npx cdk

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ if [[ $# -eq 1 ]]; then
     export CDK_DEPLOY_REGION=$1
     echo "Deploying main application stack in $CDK_DEPLOY_REGION"
     echo "Deploying web application stack in us-east-1"
-    cdk deploy --all --require-approval never
+    npx cdk deploy --all --require-approval never
     exit $?
 else
     echo 1>&2 "Expecting one input parameter; please provide region (e.g. us-east-1) to deploy stack to."
