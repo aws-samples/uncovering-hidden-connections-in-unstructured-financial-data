1fde70a0673b305e54a6118d13f7dc0569aea218
Xan Huang <xan.huang@gmail.com>
2024-11-16 20:41:32 +0800

    default deployment to us-east-1 if not specified

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
-if [[ $# -eq 1 ]]; then
-    export CDK_DEPLOY_REGION=$1
+if [[ $# -eq 0 || $# -eq 1 ]]; then
+    if [[ $# -eq 0 ]]; then
+        export CDK_DEPLOY_REGION="us-east-1" #default to us-east-1
+    else
+        export CDK_DEPLOY_REGION=$1
+    fi
     echo "Deploying main application stack in $CDK_DEPLOY_REGION"
     echo "Deploying web application stack in us-east-1"
     npx cdk deploy --all --require-approval never
